2a568da644c17d724c64b8167329085a867c8e69
Harshal Bhute <harshalbhute39@gmail.com>
2025-02-11 15:40:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+  let newStr = str.split(' ');
+	if(newStr === str){
+		console.log(`{str} is a pallindrome`);
+	}
+	else{
+		console.log(`{str} is not a pallindrome`);
+	}
 }
 module.exports = palindrome
